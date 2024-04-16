1b08498db6e9c71f2ab3421609280dcd909b69fe
AYT04 <evoker4mc@icloud.com>
2024-04-16 09:36:53 -0400

    UI Change

---
+++ WhatYearIsIt.js
@@ -0,0 +1,8 @@
+  // Get the current date
+  var currentDate = new Date();
+  
+    // Get the current year
+  var currentYear = currentDate.getFullYear();
+  
+    // Display the current year on the web page
+  document.write("AYTClips 2004-" + currentYear);
